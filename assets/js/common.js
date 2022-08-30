9e5ff3fa7bb1f9387ec20534d90f7be4f45d6e40
James <5000win@naver.com>
2022-08-30 23:46:45 +0900

    파일정리

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,135 +1,72 @@
-/*
-CSS Browser Selector v0.4.0 (Nov 02, 2010)
-Rafael Lima (http://rafael.adm.br)
-http://rafael.adm.br/css_browser_selector
-License: http://creativecommons.org/licenses/by/2.5/
-Contributors: http://rafael.adm.br/css_browser_selector#contributors
-*/
-function css_browser_selector(u){var ua=u.toLowerCase(),is=function(t){return ua.indexOf(t)>-1},g='gecko',w='webkit',s='safari',o='opera',m='mobile',h=document.documentElement,b=[(!(/opera|webtv/i.test(ua))&&/msie\s(\d)/.test(ua))?('ie ie'+RegExp.$1):is('firefox/2')?g+' ff2':is('firefox/3.5')?g+' ff3 ff3_5':is('firefox/3.6')?g+' ff3 ff3_6':is('firefox/3')?g+' ff3':is('gecko/')?g:is('opera')?o+(/version\/(\d+)/.test(ua)?' '+o+RegExp.$1:(/opera(\s|\/)(\d+)/.test(ua)?' '+o+RegExp.$2:'')):is('konqueror')?'konqueror':is('blackberry')?m+' blackberry':is('android')?m+' android':is('chrome')?w+' chrome':is('iron')?w+' iron':is('applewebkit/')?w+' '+s+(/version\/(\d+)/.test(ua)?' '+s+RegExp.$1:''):is('mozilla/')?g:'',is('j2me')?m+' j2me':is('iphone')?m+' iphone':is('ipod')?m+' ipod':is('ipad')?m+' ipad':is('mac')?'mac':is('darwin')?'mac':is('webtv')?'webtv':is('win')?'win'+(is('windows nt 6.0')?' vista':''):is('freebsd')?'freebsd':(is('x11')||is('linux'))?'linux':'','js']; c = b.join(' '); h.className += ' '+c; return c;}; css_browser_selector(navigator.userAgent);
+$(function () {
+  setTimeout(function () {
+    $("#wrap").addClass("intro");
+  }, 0);
 
-/* ie 버그로 인하여 강제로 탭키 가도록 설정
--------------------------------------------------*/
-jQuery(document).ready(function(){
-	if(is_IE()){
-		jQuery("#gnavigation").prop("tabindex", "0");
-		jQuery("#body").prop("tabindex", "0");
-		jQuery("#contents").prop("tabindex", "0");
-	 }
-});
-// IE 인지를 반환(IE 라면 true 아니면 false)
-function is_IE() {
-	if((navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || navigator.appName.charAt(0) == "M"){
-		return true ;
-	}else{
-		return false ;
-	}
-}
-//확대 축소
-$(document).ready(function() {
-	var nowZoom = 1;
-	$("#btnFontSizeSubout").click(function(){
-		nowZoom /= 1.05;
-	    zooms(nowZoom);
-	});
+  //햄버거매뉴(모바일)
+  $(".menu-btn").click(function () {
+    $(this).toggleClass("active");
+    $(this).next().toggleClass("active");
+    if ($(window).width() < 767) {
+      $("body").toggleClass("fixed");
+    }
+  });
 
-	$("#btnFontSizeSubin").click(function(){
-		nowZoom *= 1.05;
-	    zooms(nowZoom);
-	});
-});
+  //학부바로가기 버튼
+  $(".department button").click(function () {
+    $(this).next().slideToggle();
+    $(this).parent().toggleClass("active");
+  });
 
-$(function(){
-	//자동으로 테이블 캡션 넣기
-	$('.table-wrap').each(function(){
-		var tableTitle = $(this).prev().text();
-		var thArrayStr = "";
-		$(this).find('thead th').each(function(i){
-			thArrayStr += ", " + $(this).text();
-		});
-		$(this).find("table caption").html('<strong>' + tableTitle + '</strong>' + '<span>' + thArrayStr.substring(2) + '에 관한 정보' + '</span>' );
-	});
+  //하단 관련사이트
+  $(".family-site").click(function () {
+    $(this).toggleClass("active");
+    $(this).find(".depth-list").slideToggle();
+  });
 
-	//대매뉴
-	$('#gnb').on('mouseenter focusin', function(){
-		$(this).parents('#header').addClass('active');
-	});
-	$('#header').on('mouseleave', function(){
-		$(this).removeClass('active');
-	});
-	$('#gnb>li:last .depth2>li:last').focusout(function(){
-		$(this).parents('#header').removeClass('active');
-	});
-	$('#header .logo').focusin(function(){
-		$('.department').removeClass('active');
-		$('.department-list').slideUp();
-		$(this).parents('#header').removeClass('active');
-	});
+  //상단으로 스무스~
+  $(".top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 400);
+    return false;
+  });
 
-	//햄버거매뉴(모바일)
-	$('.menu-btn').click(function(){
-		$(this).toggleClass('active');
-		$(this).next().toggleClass('active');
-		if ($(window).width()<767) {
-			$('body').toggleClass('fixed');
-		}
-	});
+  //모달팝업
+  $(".modal-trigger").click(function () {
+    $(this).next().addClass("active");
+    $("body").addClass("fixed");
+  });
+  $("#modal .close").click(function () {
+    $(this).parents("#modal").removeClass("active");
+    $("body").removeClass("fixed");
+  });
 
-	//학부바로가기 버튼
-	$('.department button').click(function(){
-		$(this).next().slideToggle();
-		$(this).parent().toggleClass('active');
-	});
+  //위치 및 연락처 (캠퍼스 2개 )
+  // 첫번째 활성화
+  $(".locate .map li.first").css("display", "block");
+  $(".locate .info > li.first").css("display", "block");
 
-	//하단 관련사이트
-	$('.family-site').click(function(){
-		$(this).toggleClass('active');
-		$(this).find('.depth-list').slideToggle();
-	});
+  $(".locate .tab li").click(function () {
+    //메뉴 활성화
+    $(this).addClass("active").siblings().removeClass("active");
+    var area_num = $(this).index();
 
-	//상단으로 스무스~
-	$('.top').click(function(){
-		$('html, body').animate({scrollTop:0},400);
-		return false;
-	});
-
-	//모달팝업
-	$('.modal-trigger').click(function(){
-		$(this).next().addClass('active');
-		$('body').addClass('fixed');
-	});
-	$('#modal .close').click(function(){
-		$(this).parents('#modal').removeClass('active');
-		$('body').removeClass('fixed');
-	});
-
-	//위치 및 연락처 (캠퍼스 2개 )
-		// 첫번째 활성화
-		$('.locate .map li.first').css('display', 'block');
-		$('.locate .info > li.first').css('display', 'block');
-
-		$(".locate .tab li").click(function() {
-			//메뉴 활성화
-			$(this).addClass("active").siblings().removeClass("active");
-			var area_num = $(this).index();
-
-			//콘텐츠 활성화
-			if($('.locate .tab li').hasClass('active') == true) {
-				$('.locate .map > li').siblings().fadeOut().eq(area_num).fadeIn();
-				$('.locate .info > li').siblings().fadeOut().eq(area_num).fadeIn();
-			}
-		});
-
+    //콘텐츠 활성화
+    if ($(".locate .tab li").hasClass("active") == true) {
+      $(".locate .map > li").siblings().fadeOut().eq(area_num).fadeIn();
+      $(".locate .info > li").siblings().fadeOut().eq(area_num).fadeIn();
+    }
+  });
 });
 
-$(window).resize(function(){
-	if ($(window).width() < 767) {
-		$('#gnb>li>a').click(function(){
-			$(this).attr('href','javascript:void(0)');
-			$(this).parent().addClass('active').siblings().removeClass('active');
-		});
-	}
+$(window).resize(function () {
+  if ($(window).width() < 767) {
+    $("#gnb>li>a").click(function () {
+      $(this).attr("href", "javascript:void(0)");
+      $(this).parent().addClass("active").siblings().removeClass("active");
+    });
+  }
 });
 
-$(window).on("load", function(){
-	$(window).trigger('resize');
+$(window).on("load", function () {
+  $(window).trigger("resize");
 });
--- assets/js/type1.js
+++ assets/js/type1.js
@@ -1,50 +1,12 @@
-$(window).scroll(function(){
-  if($(window).scrollTop()>500){
-    $('.nav-area').addClass('sticky');
-  }else{
-    $('.nav-area').removeClass('sticky');
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 500) {
+    $(".nav-area").addClass("sticky");
+  } else {
+    $(".nav-area").removeClass("sticky");
   }
 });
 
-$(function(){
-  //비주얼슬라이드
-  $('.visual-slide').bxSlider({
-  auto:true,
-  speed:1000,
-  pause:5000,
-  mode:'fade',
-  controls:false,
-  onSlideAfter:function($slideElement, oldIndex, newIndex){
-    $('.visual-slide>li').removeClass('active');
-    $('.visual-slide>li').eq(newIndex).addClass('active');
-  }
-  });
-  $(".visual-slide>li.first").addClass("active");
-
-  $('.photo-list').bxSlider({
-    auto:false,
-    mode:'horizontal',
-    controls:true,
-    pager:false,
-    // pagerType: 'short',
-    moveSlides:1,
-    maxSlides: 2,
-    slideWidth: 280,
-    slideMargin: 30,
-  });
-
-  //게시판
-  $('.brief-list>li>a').click(function(){
-    $(this).parent().addClass('active').siblings().removeClass('active');
-  });
-
-  //서브레이아웃 네비
-	$('.depth1>li>a').click(function(){
-		$(this).next().slideToggle();
-		$(this).parent().siblings().find('.depth2').slideUp();
-		$(this).parent().toggleClass('active').siblings().removeClass('active');
-	});
-
+$(function () {
   // Controller 객체 설정
   var controller = new ScrollMagic.Controller();
 
@@ -52,54 +14,52 @@ $(function(){
   // // // 핀 컨트롤(제어) //
   // // ////////////////////
   var carousel_pin = new ScrollMagic.Scene({
-    'triggerElement': '.pin',
-    'triggerHook':0
+    triggerElement: ".pin",
+    triggerHook: 0,
     // 'location': '35%'
-  })
+  });
   carousel_pin
-    .setPin('.pin')
+    .setPin(".pin")
     // .addIndicators()
     .addTo(controller);
 
   //////////////////////////////
   // 페럴렉스 씬 제어 반복 구문 //
   //////////////////////////////
-  var scene_list = '.section1, .section2, .section3'.split(', ');
-  scene_list.forEach(function(trigger_el_selector, idx){
-
+  var scene_list = ".section1, .section2, .section3".split(", ");
+  scene_list.forEach(function (trigger_el_selector, idx) {
     var scene = new ScrollMagic.Scene({
-      'triggerElement': trigger_el_selector,
-      'triggerHook':0.5, //트리거의 시작지점 0 ~ 1
-      'offset':-250,
+      triggerElement: trigger_el_selector,
+      triggerHook: 0.5, //트리거의 시작지점 0 ~ 1
+      offset: -250,
       // 'reverse': false
     })
 
-    .setClassToggle(trigger_el_selector, 'fade-in')
-    // .addIndicators({
-    //   'name' : trigger_el_selector
-    // })
-    .addTo(controller);
+      .setClassToggle(trigger_el_selector, "fade-in")
+      // .addIndicators({
+      //   'name' : trigger_el_selector
+      // })
+      .addTo(controller);
   });
 
   //메인컨텐츠 전체 올라오기
-  var tween = TweenMax.to("#contents", 1, {css:{transform:"translateY(-104px)"}});
-  var scene = new ScrollMagic.Scene({offset:200 ,duration:500})
-      .setTween(tween)
-      // .addIndicators(true)
-      .addTo(controller);
+  var tween = TweenMax.to("#contents", 1, {
+    css: { transform: "translateY(-104px)" },
+  });
+  var scene = new ScrollMagic.Scene({ offset: 200, duration: 500 })
+    .setTween(tween)
+    // .addIndicators(true)
+    .addTo(controller);
 
   //텍스트 애니메이션
-  var tween2 = TweenMax.to(".type-animate", 1, {css:{opacity:"1"}});
-  var scene = new ScrollMagic.Scene({offset:200 ,duration:500})
-      .setTween(tween2)
-      .addTo(controller);
-
+  var tween2 = TweenMax.to(".type-animate", 1, { css: { opacity: "1" } });
+  var scene = new ScrollMagic.Scene({ offset: 200, duration: 500 })
+    .setTween(tween2)
+    .addTo(controller);
 
   //해당 학부 상징물
-  var tween3 = TweenMax.to(".object", 1, {css:{opacity:"1"}});
-  var scene = new ScrollMagic.Scene({offset:550 ,duration:600})
-      .setTween(tween3)
-      .addTo(controller);
-
-
+  var tween3 = TweenMax.to(".object", 1, { css: { opacity: "1" } });
+  var scene = new ScrollMagic.Scene({ offset: 550, duration: 600 })
+    .setTween(tween3)
+    .addTo(controller);
 });
